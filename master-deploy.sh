4706a4684438f0ba1e84bdf35fc8ef74bc42c754
ChenPenguin <54030265+ChenPenguin@users.noreply.github.com>
2021-12-11 13:17:16 +0800

    Delete master-deploy.sh

--- master-deploy.sh
+++
@@ -1,9 +0,0 @@
-# Master节点初始化
-kubeadm init --apiserver-advertise-address=10.10.11.50 --image-repository registry.aliyuncs.com/google_containers --kubernetes-version v1.19.4 --service-cidr=10.96.0.0/12 --pod-network-cidr=10.244.0.0/16
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-Master节点部署网络插件
-wget https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-kubectl apply -f kube-flannel.yml
